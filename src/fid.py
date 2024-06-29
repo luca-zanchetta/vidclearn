7e49c1c547884d4d2aef629613c81d4f910496c8
Luca Zanchetta <zanchetta.1848878@studenti.uniroma1.it>
2024-06-29 15:25:46 +0200

    hotfix

--- src/fid.py
+++ src/fid.py
@@ -6,13 +6,15 @@ from tqdm import tqdm
 
 # Function for extracting frames from video
 def extract_frames_from_video(video, target_size=(299, 299)):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     frames = video.permute(1, 2, 3, 0).numpy()
     frames = [cv2.resize(frame, target_size) for frame in frames]
-    frames = [torch.tensor(frame, dtype=torch.uint8).permute(2, 0, 1).unsqueeze(0).to('cuda') for frame in frames]
+    frames = [torch.tensor(frame, dtype=torch.uint8).permute(2, 0, 1).unsqueeze(0).to(device) for frame in frames]
     return frames
 
 def compute_fid(real_videos, generated_videos, batch_size=32):
-    fid = FrechetInceptionDistance(feature=64).to('cuda')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    fid = FrechetInceptionDistance(feature=64).to(device)
     
     for video in tqdm(real_videos, total=len(real_videos)):
         frames = extract_frames_from_video(video)
