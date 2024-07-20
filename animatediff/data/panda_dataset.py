169fb9b6bdb06940fb17cd011c8414fa9dbaffa2
Luca Zanchetta <zanchetta.1848878@studenti.uniroma1.it>
2024-07-20 17:33:36 +0200

    Fix pytube error
    Special thanks to https://github.com/pytube/pytube/issues/1973#issuecomment-2232578734 <3

--- animatediff/data/panda_dataset.py
+++ animatediff/data/panda_dataset.py
@@ -9,6 +9,14 @@ from pytube import YouTube
 from pytube.exceptions import PytubeError
 from datetime import datetime
 from moviepy.video.io.VideoFileClip import VideoFileClip
+from pytube. innertube import _default_clients
+
+_default_clients[ "ANDROID"][ "context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients[ "ANDROID_EMBED"][ "context"][ "client"]["clientVersion"] = "19.08.35"
+_default_clients[ "IOS_EMBED"][ "context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_MUSIC"][ "context"]["client"]["clientVersion"] = "6.41"
+_default_clients[ "ANDROID_MUSIC"] = _default_clients[ "ANDROID_CREATOR" ]
 
 def on_progress(stream, chunk, bytes_remaining):
     total_size = stream.filesize
