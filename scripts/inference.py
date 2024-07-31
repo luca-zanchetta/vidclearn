fe02fd3ab4915986f64728d099a0140465a0a6dc
Luca Zanchetta <zanchetta.1848878@studenti.uniroma1.it>
2024-07-31 17:37:42 +0200

    Update inference.py
    Modularity update

--- scripts/inference.py
+++ scripts/inference.py
@@ -1,17 +1,36 @@
+import torch
 from tuneavideo.pipelines.pipeline_tuneavideo import TuneAVideoPipeline
 from tuneavideo.models.unet import UNet3DConditionModel
 from tuneavideo.util import save_videos_grid
-import torch
 
+# Hyperparameters
 pretrained_model_path = "./checkpoints/stable-diffusion-v1-4"
-my_model_path = "./outputs/man-skiing"
-unet = UNet3DConditionModel.from_pretrained(my_model_path, subfolder='unet', torch_dtype=torch.float16).to('cuda')
+model_path = "./outputs/man-skiing"
+prompt = "spider man is skiing"
+save_path = f"./inference_samples/1-{prompt}.gif"
+frames_per_video = 24
+height = 512
+width = 512
+inference_steps = 50
+guidance_scale = 12.5
+
+# Setup model
+unet = UNet3DConditionModel.from_pretrained(model_path, subfolder='unet', torch_dtype=torch.float16).to('cuda')
 pipe = TuneAVideoPipeline.from_pretrained(pretrained_model_path, unet=unet, torch_dtype=torch.float16).to("cuda")
 pipe.enable_xformers_memory_efficient_attention()
 pipe.enable_vae_slicing()
 
-prompt = "spider man is skiing"
-ddim_inv_latent = torch.load(f"{my_model_path}/inv_latents/ddim_latent-500.pt").to(torch.float16)
-video = pipe(prompt, latents=ddim_inv_latent, video_length=24, height=512, width=512, num_inference_steps=50, guidance_scale=12.5).videos
+# Perform inference
+ddim_inv_latent = torch.load(f"{model_path}/inv_latents/ddim_latent-500.pt").to(torch.float16)
+video = pipe(
+    prompt, 
+    latents=ddim_inv_latent, 
+    video_length=frames_per_video,
+    height=height,
+    width=width, 
+    num_inference_steps=inference_steps,
+    guidance_scale=12.5
+).videos
 
-save_videos_grid(video, f"./inference_samples/{prompt}.gif")
+# Save generated video
+save_videos_grid(video, save_path)
